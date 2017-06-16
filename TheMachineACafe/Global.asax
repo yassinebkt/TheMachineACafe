6ed8694f848b0224fac472e36651a0dd7557a4b8
Yassine <yassine753357@gmail.com>
2017-06-16 15:00:16 +0200

    Initial commit

---
+++ TheMachineACafe/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TheMachineACafe.MvcApplication" Language="C#" %>
---
+++ TheMachineACafe/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheMachineACafe.Startup))]
+namespace TheMachineACafe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
