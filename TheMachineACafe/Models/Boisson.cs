a319575d24fb74a1bb0c5c49ba4aeadff9d154a5
Yassine <yassine753357@gmail.com>
2017-06-16 15:31:51 +0200

    Commit the Model

---
+++ TheMachineACafe/Models/Boisson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TheMachineACafe.Models
+{
+    public class Boisson
+    {
+        public int Id { get; set; }
+
+
+        [Required]
+        public string Nom { get; set; }
+    }
+}
---
+++ TheMachineACafe/Models/Selection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TheMachineACafe.Models
+{
+    public class Selection
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Selectionner un boisson")]
+        [Required]
+        public int BoissonId { get; set; }
+
+        public ApplicationUser UserId { get; set; }
+
+        [Display(Name = "Sucre")]
+        [Required]
+        public int Sucre { get; set; }
+
+        [Display(Name = "Muge")]
+        [Required]
+        public bool HasMuge { get; set; }
+
+        public virtual Boisson boisson { get; set; }
+    }
+}
